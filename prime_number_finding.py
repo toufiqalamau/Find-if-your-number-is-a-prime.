b677089fb30d986a8de6dbbd5bcfb424786c7c16
toufiqalamau <toufiqalamau@gmail.com>
2017-05-25 16:00:44 +0600

    find if a particular number is prime.
    Find out if your number is prime.

--- prime_number_finding.py
+++ prime_number_finding.py
@@ -1,22 +1,17 @@
-inpt=int(raw_input('give me a number bigger than 0 and I shall give you a list of primes from 1 upto your number: '))
+i=int(raw_input('Is your number a prime number? type it here to find out. exclude 2: '))
+
+for x in range(2,10):
+    if i%x==0:
+        if i==2:
+            print 'prime.'
+            break
+        print'it is a composite number.'
+        break
+        
+    else:
+        print 'prime'
+        break
     
-l=range(inpt)
-l.remove(0)
-prime=[]
-prml=[1,2,3,5,7]
 
-
-for i in prml:
-    prime.append(i)
-
-
-for i in l:
-    if i%2!=0:
-        if i%3!=0:
-            if i%5!=0:
-                if i%7!=0:
-                    prime.append(i)
-                
-prime.remove(1)
-prime.sort()
-print prime        
+#this program has a major flaw...when the input is from 1-10, it will give 'composite'
+#result even for the prime numbers.
